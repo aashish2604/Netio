2b6d49b2558923661b438a4cccb287849e3d60db
aashish2604 <aashish.260401@gmail.com>
2023-03-15 03:43:30 +0530

    feat: added options for api call

---
+++ lib/src/options.dart
@@ -0,0 +1,81 @@
+typedef ProgressCallback = void Function(int count, int total);
+
+enum ListFormat {
+  /// Comma-separated values
+  /// e.g. (foo,bar,baz)
+  csv,
+
+  /// Space-separated values
+  /// e.g. (foo bar baz)
+  ssv,
+
+  /// Tab-separated values
+  /// e.g. (foo\tbar\tbaz)
+  tsv,
+
+  /// Pipe-separated values
+  /// e.g. (foo|bar|baz)
+  pipes,
+
+  /// Multiple parameter instances rather than multiple values.
+  /// e.g. (foo=value&foo=another_value)
+  multi,
+
+  /// Forward compatibility
+  /// e.g. (foo[]=value&foo[]=another_value)
+  multiCompatible,
+}
+
+typedef ValidateStatus = bool Function(int? status);
+
+enum ResponseType {
+  /// Transform the response data to JSON object only when the
+  /// content-type of response is "application/json" .
+  json,
+
+  /// Get the response stream without any transformation. The
+  /// Response data will be a `ResponseBody` instance.
+  stream,
+
+  /// Transform the response data to a String encoded with UTF8.
+  plain,
+
+  /// Get original bytes, the type of [Response.data] will be List<int>
+  bytes
+}
+
+class Options {
+  String? method;
+  Duration? connectTimeout;
+  Duration? receiveTimeout;
+  Duration? sendTimeout;
+  String baseUrl;
+  Map<String, dynamic>? queryParameters;
+  Map<String, dynamic>? extra;
+  Map<String, dynamic>? headers;
+  ResponseType? responseType;
+  ValidateStatus? validateStatus;
+  bool? receiveDataWhenStatusError;
+  bool? followRedirects;
+  int? maxRedirects;
+  bool? persistentConnection;
+  ListFormat? listFormat;
+
+  Options({
+    this.method,
+    this.connectTimeout,
+    this.receiveTimeout,
+    this.sendTimeout,
+    this.baseUrl = '',
+    this.queryParameters,
+    this.extra,
+    this.headers,
+    this.responseType = ResponseType.json,
+    this.validateStatus,
+    this.receiveDataWhenStatusError,
+    this.followRedirects,
+    this.maxRedirects,
+    this.persistentConnection,
+    this.listFormat,
+  });
+}
